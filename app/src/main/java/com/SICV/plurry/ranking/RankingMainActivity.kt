04ac873dbcd64b4e0b6309897e882df4a38e77fd
LeeSeul <leeseul2021@duksung.ac.kr>
2025-04-02 14:10:41 +0900

    프로젝트 기본틀 생성
    각 기능 메인액티비티 페이지 생성 및 프로젝트 메인 액티비티에 버튼연결

---
+++ app/src/main/java/com/SICV/plurry/ranking/RankingMainActivity.kt
@@ -0,0 +1,12 @@
+package com.SICV.plurry.ranking
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.SICV.plurry.R
+
+class RankingMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ranking_main)
+    }
+}
