ebf3d6066b0a7f1734798dd54531156d4458e262
Kdaae1214 <162955897+Kdaae1214@users.noreply.github.com>
2025-04-10 22:35:48 +0900

    액티비티 추가

---
+++ app/src/main/java/com/SICV/plurry/goingwalk/EndWalkActivity.kt
@@ -0,0 +1,12 @@
+package com.SICV.plurry.goingwalk
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.SICV.plurry.R
+
+class EndWalkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_going_walk_main)
+    }
+}
