d93e5aaf50baccb172f2c058c45133e79ae235ae
Kdaae1214 <162955897+Kdaae1214@users.noreply.github.com>
2025-04-11 14:46:10 +0900

    mapview 액티비티 및 레이아웃 생성

---
+++ app/src/main/java/com/SICV/plurry/goingwalk/MapViewActivity.kt
@@ -0,0 +1,12 @@
+package com.SICV.plurry.goingwalk
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.SICV.plurry.R
+
+class MapViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map_view)
+    }
+}
