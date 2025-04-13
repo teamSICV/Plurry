231179091822f8ad6718405e5754ab2bc9fd8e8c
Kdaae1214 <162955897+Kdaae1214@users.noreply.github.com>
2025-04-14 00:28:50 +0900

    mapview 액티비티 및 레이아웃 생성

--- app/src/main/java/com/SICV/plurry/goingwalk/GoingWalkMainActivity.kt
+++ app/src/main/java/com/SICV/plurry/goingwalk/GoingWalkMainActivity.kt
@@ -1,6 +1,8 @@
 package com.SICV.plurry.goingwalk
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.SICV.plurry.R
 
@@ -8,5 +10,11 @@ class GoingWalkMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_going_walk_main)
+
+        val startWalkButton = findViewById<Button>(R.id.startWalkButton)
+        startWalkButton.setOnClickListener {
+            val intent = Intent(this, MapViewActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/SICV/plurry/goingwalk/MapViewActivity.kt
+++ app/src/main/java/com/SICV/plurry/goingwalk/MapViewActivity.kt
@@ -1,12 +1,24 @@
 package com.SICV.plurry.goingwalk
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.SICV.plurry.R
 
 class MapViewActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map_view)
+
+        val btnEndWalk = findViewById<Button>(R.id.btnEndWalk)
+
+        btnEndWalk.setOnClickListener {
+            val intent = Intent(this, GoingWalkMainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
+            finish()
+        }
     }
 }
