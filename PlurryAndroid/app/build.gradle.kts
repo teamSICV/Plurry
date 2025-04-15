d3ad77518c3954c02e8751055cb8f5ccd3aeb32f
LeeSeul <leeseul2021@duksung.ac.kr>
2025-04-16 00:24:53 +0900

    빌드오류 해결

--- PlurryAndroid/app/build.gradle.kts
+++ PlurryAndroid/app/build.gradle.kts
@@ -60,9 +60,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-<<<<<<< HEAD
     implementation ("com.github.philjay:MPAndroidChart:3.1.0")
-=======
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
@@ -70,7 +68,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
->>>>>>> kdaae_dev
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
