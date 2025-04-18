50b238722c1ec827f750390e8df40a3713653066
LeeSeul <127869350+LeeSeul0473@users.noreply.github.com>
2025-04-18 16:33:41 +0900

    유니티 액티비티 수정 추가

--- PlurryAndroid/app/src/main/java/com/SICV/plurry/MainActivity.kt
+++ PlurryAndroid/app/src/main/java/com/SICV/plurry/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.SICV.plurry
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.SICV.plurry.crewstep.CrewLineMainActivity
 import com.SICV.plurry.goingwalk.GoingWalkMainActivity
