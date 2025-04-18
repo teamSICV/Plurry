5e64daf13b5c6cd2a8fd2c0cb3719de0253b98b0
LeeSeul <leeseul2021@duksung.ac.kr>
2025-04-18 15:53:20 +0900

    유니티연결-shared폴더 등 추가

--- PlurryAndroid/settings.gradle.kts
+++ PlurryAndroid/settings.gradle.kts
@@ -21,10 +21,11 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven(url = "https://jitpack.io")
-    }
 
-    flatDir {
-        dirs("${project(":unityLibrary").projectDir}/libs")
+        flatDir {
+            dirs("${project(":unityLibrary").projectDir}/libs")
+        }
+
     }
 
 }
