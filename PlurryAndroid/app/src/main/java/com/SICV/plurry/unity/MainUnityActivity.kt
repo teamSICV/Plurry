01c4f2c4bcee9475a36a67d077828a94f72703d0
LeeSeul <leeseul2021@duksung.ac.kr>
2025-04-28 21:01:43 +0900

    유니티액티비티 유니티메인액티비티 안으로 집어넣음

--- PlurryAndroid/app/src/main/java/com/SICV/plurry/unity/MainUnityActivity.kt
+++ PlurryAndroid/app/src/main/java/com/SICV/plurry/unity/MainUnityActivity.kt
@@ -3,39 +3,87 @@ package com.SICV.plurry.unity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import com.unity3d.player.UnityPlayerActivity
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.SICV.plurry.R
 
-class MainUnityActivity : UnityPlayerActivity() {
+class MainUnityActivity : AppCompatActivity() {
+
+    //Unity Properties
+    var isUnityLoaded: Boolean = false
+    private lateinit var mGoUnityButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("Logsicv", "MainUnityActivity onCreate")
-
-
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_unity_main)
 
-        // Setup activity layout
-        SharedClass.addControlsToUnityFrame(this)
-        val intent = intent
-        handleIntent(intent)
+        //Unity Settings
+        setupUnityButtons()
     }
 
-
+    //Unity Methods
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
-        handleIntent(intent)
         setIntent(intent)
     }
 
-    fun handleIntent(intent: Intent?) {
-        if (intent == null || intent.extras == null) return
+    private fun startUnityWithClass(klass: Class<*>?) {
+        if (klass == null) {
+            showToast("Unity Activity not found")
+            return
+        }
+        val intent = Intent(this, klass)
+        intent.flags = Intent.FLAG_ACTIVITY_REORDER_TO_FRONT
+        startActivityForResult(intent, 1)
+    }
 
-        if (intent.extras!!.containsKey("doQuit")) {
-            if (mUnityPlayer != null) {
-                finish()
-            }
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1) {
+            isUnityLoaded = false
+            //enableShowUnityButtons()
+            showToast("Unity finished.")
         }
     }
 
-    override fun onUnityPlayerUnloaded() {
-        SharedClass.showMainActivity("")
+    private fun showToast(message: String) {
+        val duration = Toast.LENGTH_SHORT
+        Toast.makeText(applicationContext, message, duration).show()
+    }
+
+    override fun onBackPressed() {
+        finishAffinity()
+    }
+
+    private fun findClassUsingReflection(className: String): Class<*>? {
+        return try {
+            Class.forName(className)
+        } catch (e: ClassNotFoundException) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+    private fun getMainUnityGameActivityClass(): Class<*>? {
+        return findClassUsingReflection("com.SICV.plurry.unity.MainUnityGameActivity")
+    }
+
+    private fun setupUnityButtons() {
+        mGoUnityButton = findViewById(R.id.b_go_unity)
+
+        if (getMainUnityGameActivityClass() != null) {
+            mGoUnityButton.visibility = View.VISIBLE
+            //mActivityType = ActivityType.PLAYER_GAME_ACTIVITY
+        }
+
+        mGoUnityButton.setOnClickListener{
+            Log.d("Logsicv", "MainActivity setOnClickListener")
+
+            isUnityLoaded = true
+            startUnityWithClass(getMainUnityGameActivityClass())
+        }
     }
 }
