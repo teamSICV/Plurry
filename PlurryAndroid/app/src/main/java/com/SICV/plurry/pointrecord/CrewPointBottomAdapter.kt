175c32c6e1e4bce644d1d23b79a28afcf499f10b
jamm2n <144921408+jamm2n@users.noreply.github.com>
2025-05-05 22:32:21 +0900

    이미지 가져오기 시도까지만
    gradle glide관련 부분, 나머지는 firebase에서 이미지 가져오려고 시도한 부분

--- PlurryAndroid/app/src/main/java/com/SICV/plurry/pointrecord/CrewPointBottomAdapter.kt
+++ PlurryAndroid/app/src/main/java/com/SICV/plurry/pointrecord/CrewPointBottomAdapter.kt
@@ -8,29 +8,34 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.SICV.plurry.R
+import com.bumptech.glide.Glide
 
-class CrewPointBottomAdapter (
+class CrewPointBottomAdapter(
     private val context: Context,
-    private val imageList: List<Int>
-    ):RecyclerView.Adapter<CrewPointBottomAdapter.ImageViewHolder>(){
+    private val imageList: List<String>
+) : RecyclerView.Adapter<CrewPointBottomAdapter.ImageViewHolder>() {
 
-        inner class ImageViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
-            val imageView: ImageView =itemView.findViewById(R.id.crewPointImage)
-        }
+    inner class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.crewPointImage)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
-        val itemView = LayoutInflater.from(context).inflate(R.layout.activity_point_record_bottom_image, parent, false)
+        val itemView = LayoutInflater.from(context)
+            .inflate(R.layout.activity_point_record_bottom_image, parent, false)
         return ImageViewHolder(itemView)
     }
 
-    override fun getItemCount(): Int {
-        return imageList.size
-    }
+    override fun getItemCount(): Int = imageList.size
 
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
-        holder.imageView.setImageResource(imageList[position])
+        val imageUrl = imageList[position]
 
-        holder.imageView.setOnClickListener{
+        Glide.with(context)
+            .load(imageUrl)
+            .placeholder(R.drawable.test1)
+            .into(holder.imageView)
+
+        holder.imageView.setOnClickListener {
             val dialog = PointRecordDialog()
             dialog.show((context as AppCompatActivity).supportFragmentManager, "CrewPointDialog")
         }
