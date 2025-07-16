ca55116242c973aab06128b97ab01f401b3d172d
jamm2n <144921408+jamm2n@users.noreply.github.com>
2025-07-16 14:46:18 +0900

    양육버튼 -> 필드 추가
    메인에서 양육버튼 눌릴 때 level 등 DB구조 생성시키기

---
+++ PlurryAndroid/app/src/main/java/com/SICV/plurry/UserRewardInitializer.kt
@@ -0,0 +1,43 @@
+package com.SICV.plurry
+
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.unity3d.player.a.e
+
+class UserRewardInitializer {
+    companion object{
+        private const val TAG = "UserRewardInitializer"
+
+        fun intializeUserReward(onSucces: ()-> Unit, onFailure: (Exception)-> Unit){
+            val currentUser = FirebaseAuth.getInstance().currentUser
+            if(currentUser == null){
+                onFailure(Exception("User not logged in"))
+                return
+            }
+
+            val uid = currentUser.uid
+            val db = FirebaseFirestore.getInstance()
+
+            val userRewardData = hashMapOf(
+                "characterName" to "",
+                "crewRewardItem" to null,
+                "level" to 0,
+                "storyLevel" to 0,
+                "userRewardItem" to 0
+            )
+
+            db.collection("Game")
+                .document("users")
+                .collection("userReward")
+                .document(uid)
+                .set(userRewardData)
+                .addOnSuccessListener {
+                    onSucces()
+                }
+                .addOnFailureListener { e->
+                    onFailure(e)
+                }
+        }
+    }
+}
